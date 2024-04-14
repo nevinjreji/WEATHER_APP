e781e2e573da5d0e80e2cd5bff7da9d610a0ffcd
nevinjreji <166946502+nevinjreji@users.noreply.github.com>
2024-04-14 22:54:50 +0530

    Update serverstart.js

--- NODE/serverstart.js
+++ NODE/serverstart.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/paymentDB')
+mongoose.connect('mongodb+srv://Dsouza:Nqtb2cS1ugkBtX7w@cluster0.bk52qvx.mongodb.net/paymentDB')
     .then(() => {
     console.log('Connected to MongoDB');
 }).catch(err => {
@@ -51,7 +51,7 @@ app.get('/getweather', (req, res) => {
 app.post('/checkPayment', async (req, res) => {
     const utrNumber = req.body.utrNumber;
 
-    const client = new MongoClient('mongodb://localhost:27017')
+    const client = new MongoClient('mongodb+srv://Dsouza:Nqtb2cS1ugkBtX7w@cluster0.bk52qvx.mongodb.net/')
     const db = client.db('paymentDB');
     const collection = db.collection('UTR'); 
 
